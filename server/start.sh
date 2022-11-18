93293df26056f8bb5c6fc1b22ea10a22f6d61432
liabri <liabri@tuta.io>
2022-11-18 12:05:00 +0100

    actually update server fabric loader & remove voyager mod

--- server/start.sh
+++ server/start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Xmx6G -jar fabric-server-mc.1.19.2-loader.0.14.9-launcher.0.11.1.jar --forceUpgrade --nogui
+java -Xmx6G -jar fabric-server-mc.1.19.2-loader.0.14.10-launcher.0.11.1.jar --forceUpgrade --nogui
